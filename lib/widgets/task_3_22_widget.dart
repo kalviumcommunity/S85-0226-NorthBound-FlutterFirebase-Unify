b958b9e1ecc5c9e8372202a9cc0d63199244156f
SHIVANG <iamshivanggautam@gmail.com>
2026-05-14 13:40:24 +0530

    Completed task 3.22

---
+++ lib/widgets/task_3_22_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class Task322Widget extends StatelessWidget {
+  const Task322Widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const Center(child: Text('Task 3.22'));
+}
