85ad7bb74edb5c4f146c9633c8dd54f59a9f334e
SHIVANG <iamshivanggautam@gmail.com>
2026-05-14 13:43:40 +0530

    Completed task 3.35

---
+++ lib/screens/task_3_35_demo.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class Task335Demo extends StatelessWidget {
+  const Task335Demo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const Center(child: Text('Demo 3.35'));
+}
